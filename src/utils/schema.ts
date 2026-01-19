a16720f11f2da722c0d8f3959fe85ea46d4c893b
C. T. Lin <chentsulin@gmail.com>
2026-01-19 13:18:47 +0800

    feat: upgrade to Zod v4 and use native JSON schema generation (#254)

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,11 +1,9 @@
 import { z } from "zod";
-import { zodToJsonSchema as zodToJsonSchemaOriginal } from "zod-to-json-schema";
 
-// TODO: use zod v4 JSON to schema to replace zod-to-json-schema when v4 is stable
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const zodToJsonSchema = (schema: Record<string, z.ZodType<any>>) => {
-  return zodToJsonSchemaOriginal(z.object(schema), {
-    rejectedAdditionalProperties: undefined,
-    $refStrategy: "none",
+  return z.object(schema).toJSONSchema({
+    io: "input",
+    target: "draft-07",
   });
 };
